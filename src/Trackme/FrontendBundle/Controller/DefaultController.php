8104382e373fd7edf7fde104c86758bc2e2c1cc4
Gonzalo Moreno <gmoreno@acid.cl>
2013-03-16 20:04:03 -0300

    page dinamo

--- src/Trackme/FrontendBundle/Controller/DefaultController.php
+++ src/Trackme/FrontendBundle/Controller/DefaultController.php
@@ -11,6 +11,23 @@ class DefaultController extends Controller
     
     public function indexAction()
     {
-        return $this->render('TrackmeFrontendBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $menu = $em->getRepository('Trackme\BackendBundle\Entity\Page')->findBy(array('enabled' => true), array('weigth' => 'ASC', 'title' => 'ASC'));
+        return $this->render('TrackmeFrontendBundle:Default:index.html.twig', array('menu' => $menu));
+    }
+    
+    public function pageAction($url)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $page = $em->getRepository('Trackme\BackendBundle\Entity\Page')->findOneBy(array('url' => $url));
+        $menu = $em->getRepository('Trackme\BackendBundle\Entity\Page')->findBy(array('enabled' => true), array('weigth' => 'ASC', 'title' => 'ASC'));
+        
+        if (!$page) {
+            throw $this->createNotFoundException('La página solicitada no existe');
+        }
+        return $this->render('TrackmeFrontendBundle:Default:page.html.twig',
+                array('page' => $page,
+                      'menu' => $menu
+                    ));
     }
 }
