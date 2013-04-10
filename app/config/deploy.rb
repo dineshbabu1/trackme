e5ae482c2ab9fcf37f51efe519bd9bb9d7ac93e5
gonzalo <gonzalo@gonzalo-mint.(none)>
2013-04-10 01:13:37 -0400

    capifony ok and running

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -14,6 +14,7 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
+set  :use_sudo,      false
 set  :keep_releases,  3
 
 set :update_vendors, true
@@ -22,5 +23,14 @@ set :use_composer, true
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
 
+set :writable_dirs,       ["app/cache", "app/logs"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true
+
+set :dump_assetic_assets, true
+set :interactive_mode, false
+
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
+set :default_shell, "TERM=dumb /bin/sh"
