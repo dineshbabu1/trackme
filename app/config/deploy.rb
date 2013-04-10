d39bbe3e44cd0d98bf16558df3f9fd993ccab517
gonzalo <gonzalo@gonzalo-mint.(none)>
2013-04-09 21:50:07 -0400

    bootstrap cache y deploy

---
+++ app/config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "trackme.cl"
+set :domain,      "198.211.109.82"
+set :deploy_to,   "/var/www/#{application}"
+set :app_path,    "app"
+
+set :repository,  "git@github.com:ronzalo/trackme.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+set :user, "root"
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+set  :keep_releases,  3
+
+set :update_vendors, true
+set :use_composer, true
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
