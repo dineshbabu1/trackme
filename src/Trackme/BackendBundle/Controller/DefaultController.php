0dab5ee1eade33981737246d7cc9cb2492b27437
Gonzalo <gmoreno@acid.cl>
2013-05-27 17:46:18 -0400

    format

--- src/Trackme/BackendBundle/Controller/DefaultController.php
+++ src/Trackme/BackendBundle/Controller/DefaultController.php
@@ -30,6 +30,7 @@ use Ivory\GoogleMap\Services\Directions\DirectionsRequest;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/hello/{name}")
      * @Template()
@@ -49,7 +50,7 @@ class DefaultController extends Controller
         $last_business = $em->getRepository('Trackme\BackendBundle\Entity\Business')->getLastBusiness();
         return $this->render('TrackmeBackendBundle:Default:dashboard_admin.html.twig', array('business' => $last_business));
     }
-    
+
     public function dashboardAction(Request $request)
     {
         $security = $this->get('security.context');
@@ -57,17 +58,17 @@ class DefaultController extends Controller
             return $this->redirect($this->generateUrl('dashboard_admin'));
         }
         $form = $this->createFormBuilder()
-        ->add('origen', 'text', array('help' => 'Calle 123 Comuna o Ciudad'))
-        ->add('destino', 'text', array('help' => 'Calle 123 Comuna o Ciudad'))
-        ->add('precio_combustible','money', array('help' => 'Solo numeros', 'currency' => 'CLP'))
-        ->add('kilometros_por_litro','text')
+            ->add('origen', 'text', array('help' => 'Calle 123 Comuna o Ciudad'))
+            ->add('destino', 'text', array('help' => 'Calle 123 Comuna o Ciudad'))
+            ->add('precio_combustible', 'money', array('help' => 'Solo numeros', 'currency' => 'CLP'))
+            ->add('kilometros_por_litro', 'text')
             // ->add('evitar_autopista', 'checkbox', array('required' => false))
             // ->add('evitar_peaje', 'checkbox', array('required' => false))
-        ->getForm();  
+            ->getForm();
 
         $result = $this->container->get('bazinga_geocoder.geocoder')
-        ->using('free_geo_ip')
-        ->geocode($this->getRequest()->server->get('REMOTE_ADDR'));
+            ->using('free_geo_ip')
+            ->geocode($this->getRequest()->server->get('REMOTE_ADDR'));
 
         $best_route = 0;
         $duration = 0;
@@ -77,13 +78,13 @@ class DefaultController extends Controller
         #$marker = new Marker();
         #$marker->setPosition(-33.424565,-70.65033, true);
         #$marker->setIcon('http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-9d7050/shapecolor-color/shadow-1/border-black/symbolstyle-contrast/symbolshadowstyle-no/gradient-no/truck-2.png');
-        
+
         $map = $this->get('ivory_google_map.map');
         $map->setStylesheetOption('width', '100%');
         $map->setStylesheetOption('height', '500px');
-        $map->setCenter($result->getLatitude(),$result->getLongitude(),true);
+        $map->setCenter($result->getLatitude(), $result->getLongitude(), true);
         // $map->setMapOption('zoom', 13);
-        
+
         $map->setLanguage('es');
 
         if ($request->isMethod('POST')) {
@@ -91,18 +92,18 @@ class DefaultController extends Controller
             if ($form->isValid()) {
                 $data = $form->getData();
                 $origen = $this->container->get('bazinga_geocoder.geocoder')
-                ->using('google_maps')
-                ->geocode($data['origen']);
-                if($origen->getLatitude() && $origen->getLongitude()){
+                    ->using('google_maps')
+                    ->geocode($data['origen']);
+                if ($origen->getLatitude() && $origen->getLongitude()) {
                     $markerOrigen = new Marker();
                     $markerOrigen->setPosition($origen->getLatitude(), $origen->getLongitude());
                     $map->addMarker($markerOrigen);
-                }    
+                }
 
                 $destino = $this->container->get('bazinga_geocoder.geocoder')
-                ->using('google_maps')
-                ->geocode($data['destino']);    
-                if($destino->getLatitude() && $destino->getLongitude()){
+                    ->using('google_maps')
+                    ->geocode($data['destino']);
+                if ($destino->getLatitude() && $destino->getLongitude()) {
                     $markerDestino = new Marker();
                     $markerDestino->setPosition($destino->getLatitude(), $destino->getLongitude());
                     $map->addMarker($markerDestino);
@@ -113,11 +114,11 @@ class DefaultController extends Controller
                 $request->setDestination($data['destino']);
                 // $request->setAvoidHighways($data['evitar_autopista']);
                 // $request->setAvoidTolls($data['evitar_peaje']);
-                
+
                 $response = $directions->route($request);
                 $routes = $response->getRoutes();
                 // Iterate each routes
-                foreach ($routes as $route) {                    
+                foreach ($routes as $route) {
                     $legs = $route->getLegs();
                     foreach ($legs as $leg) {
 
@@ -127,20 +128,19 @@ class DefaultController extends Controller
                         $estimate = $leg->getDistance()->getValue() / 1000;
                         $estimate = $estimate / $data['kilometros_por_litro'];
                         $estimate = $estimate * $data['precio_combustible'];
-                        
+
                         $overviewPolyline = $route->getOverviewPolyline();
-                        $map->addEncodedPolyline($overviewPolyline);                           
-                        
+                        $map->addEncodedPolyline($overviewPolyline);
                     }
                     $map->setCenter($route->getBound()->getCenter());
-                    $map->setMapOption('zoom', 12);
+                    $map->setMapOption('zoom', 11);
                 }
-                
             }
-        }else{
-            $map->setMapOption('zoom', 13);
+        } else {
+            $map->setMapOption('zoom', 11);
         }
 
-        return $this->render('TrackmeBackendBundle:Default:dashboard.html.twig', array('estimate' => $estimate,'duration' => $duration, 'distance' => $distance,'map' => $map, 'form' => $form->createView()));
+        return $this->render('TrackmeBackendBundle:Default:dashboard.html.twig', array('estimate' => $estimate, 'duration' => $duration, 'distance' => $distance, 'map' => $map, 'form' => $form->createView()));
     }
+
 }
