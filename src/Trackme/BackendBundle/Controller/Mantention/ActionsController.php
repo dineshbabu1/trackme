2ace845f5d93c5f2cbd2e982c3d3106ac1f988fe
gonzalo <goncab380@hotmail.com>
2013-06-24 03:13:37 -0400

    update generators

---
+++ src/Trackme/BackendBundle/Controller/Mantention/ActionsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Trackme\BackendBundle\Controller\Mantention;
+
+use Admingenerated\TrackmeBackendBundle\BaseMantentionController\ActionsController as BaseActionsController;
+
+class ActionsController extends BaseActionsController
+{
+}
---
+++ src/Trackme/BackendBundle/Controller/Vehicle/ActionsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Trackme\BackendBundle\Controller\Vehicle;
+
+use Admingenerated\TrackmeBackendBundle\BaseVehicleController\ActionsController as BaseActionsController;
+
+class ActionsController extends BaseActionsController
+{
+}
