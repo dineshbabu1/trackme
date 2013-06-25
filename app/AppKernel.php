df25221cae39d3db8a47b2e6565dc61ef7aaa746
Gonzalo <gmoreno@acid.cl>
2013-06-25 19:39:52 -0400

    new relic setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,7 +39,8 @@ class AppKernel extends Kernel
             new Bazinga\Bundle\GeocoderBundle\BazingaGeocoderBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
-            new CodeMeme\IronMqBundle\CodeMemeIronMqBundle()            
+            new CodeMeme\IronMqBundle\CodeMemeIronMqBundle() ,
+            new Ekino\Bundle\NewRelicBundle\EkinoNewRelicBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
