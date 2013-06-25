2cef532cc9e877dc2517a88def143e2de7cca636
Gonzalo <gmoreno@acid.cl>
2013-06-25 19:59:15 -0400

    plan, ticket generators

---
+++ src/Trackme/BackendBundle/Controller/Plan/ActionsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Trackme\BackendBundle\Controller\Plan;
+
+use Admingenerated\TrackmeBackendBundle\BasePlanController\ActionsController as BaseActionsController;
+
+class ActionsController extends BaseActionsController
+{
+}
