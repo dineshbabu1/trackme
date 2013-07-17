14dad0dfc8e4a8e06e5f6bf39ee3fb886c895e1b
Gonzalo <gmoreno@acid.cl>
2013-07-17 12:46:36 -0400

    license

--- src/Trackme/BackendBundle/Controller/Page/ListController.php
+++ src/Trackme/BackendBundle/Controller/Page/ListController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the TrackmeBackendBundle package.
+ *
+ * (c) Gonzalo Moreno C. <goncab380@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Trackme\BackendBundle\Controller\Page;
 
 use Admingenerated\TrackmeBackendBundle\BasePageController\ListController as BaseListController;
--- src/Trackme/BackendBundle/Entity/Business.php
+++ src/Trackme/BackendBundle/Entity/Business.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the TrackmeBackendBundle package.
  *
- * (c) Gonzalo Moreno <goncab380@hotmail.com>
+ * (c) Gonzalo Moreno C. <goncab380@hotmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
